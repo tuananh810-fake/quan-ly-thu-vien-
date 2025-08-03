a84b76ecd4304af91b5725006ccf8535ee911c86
Quanhxoan25 <23020781@vnu.edu.vn>
2025-08-04 00:12:33 +0700

    commit

--- src/services/FileStorageService.java
+++ src/services/FileStorageService.java
@@ -2,4 +2,38 @@ package services;
 
 public class FileStorageService {
     
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import models.Document;
+
+public class FileStorageService implements IStorageService {
+    private final String FILE_PATH = "document.txt";
+
+    @Override
+    public void save(List<Document> documents) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            for (Document doc : documents) {
+                writer.write(doc.toString());
+                writer.newLine();
+            }
+            System.out.println("Luu tai lieu thanh cong");
+        } catch (IOException e) {
+            System.err.println("Loi khi mo tai lieu" + e.getMessage());
+        }
+    }
+
+    public List<Document> loaDocuments() {
+        List<Document> documents = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                documents.add(Document.fromString(line));
+            }
+        } catch (IOException e) {
+            System.err.println("Loi khu mo tai lieu" + e.getMessage());
+        }
+        return documents;
+    }
+
 }
--- src/services/IStorageService.java
+++ src/services/IStorageService.java
@@ -3,3 +3,10 @@ package services;
 public class IStorageService {
     
 }
+import java.util.List;
+import models.Document;
+
+public interface IStorageService {
+    void save(List<Document> documents);
+    List<Document> loadDocuments();
+}
